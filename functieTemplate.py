9134102302e8171a749c333cc4b677e458500925
Sabshine <sab.shine.art@gmail.com>
2022-09-21 14:41:55 +0100

    fixed merging two functions into one file

--- functieTemplate.py
+++ functieTemplate.py
@@ -16,7 +16,7 @@ def templateMatching(frame):
   templateFrame = cv2.resize(templateFrame, (768, 512))
   res = cv2.matchTemplate(templateFrame,template,method)
   
-  data = []
+  data = [(0,0)]
   
   maxv = np.max(res)
   if maxv>0.80:
@@ -33,19 +33,19 @@ def templateMatching(frame):
     # Getting centre of Among us
     rectx, recty = ((top_left[0]+bottom_right[0])/2, (top_left[1]+bottom_right[1])/2)
     rectcenter = int(rectx),int(recty)
-    data.append(rectcenter)
+    data[0] = rectcenter
     # Show centre in frame
-    cv2.circle(templateFrame, rect1center, 20, (255,255,255), 3)
+    cv2.circle(templateFrame, rectcenter, 20, (255,255,255), 3)
 
     template = templateFrame[top_left[1]:top_left[1]+h,top_left[0]:top_left[0]+w]
     data.append(templateFrame)
-  return data
+  return templateFrame, data
 
 #------------------------ Laser ----------------------------
 def laserDetection(frame):
   #change the color space
   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-  laserFrame = frame
+  laserFrame = frame.copy()
 
   #Lower boundary values for HSV
   #Upper boundary values for HSV
@@ -57,8 +57,8 @@ def laserDetection(frame):
   
   #create a circle around the mask within the frame
   cv2.circle(laserFrame, maxLoc, 20, (255, 255, 255), 2, cv2.LINE_AA)
-  cv2.imshow('Track Laser', laserFrame)
   #maxLoc is the x,y values of the drawn circle around the laser
+
   return laserFrame, maxLoc
 
 #------------------------ Main ----------------------------
@@ -77,12 +77,14 @@ while(cap.isOpened()):
   
   if ret == True:
     # Funcion calls here
-    templateMatchingFrame, rectangleCentre = templateMatching(frame)
-    laserDetectionFrame, circleCentre = laserDetection(frame)
-     
+    templateFrame, rectcenter = templateMatching(frame)
+    laserFrame, circleCentre = laserDetection(frame)
+    
+    print(rectcenter[0])
+
     # Display the resulting frame
-    cv2.imshow('Frame Template Matching', templateMatchingFrame)
-    cv2.imshow('Frame Laser', laserDetectionFrame)
+    cv2.imshow('Frame Template Matching', templateFrame)
+    cv2.imshow('Frame Laser', laserFrame)
 
     # Press Q on keyboard to  exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
